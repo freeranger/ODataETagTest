ad24fae06d14047c5c9dd89373c77eaa4693c4eb
freeranger <freeranger@users.noreply.github.com>
2018-06-15 13:13:31 +0100

    "Minimal project to demonstrate problem"

---
+++ ODataETagTest/Controllers/ValuesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNet.OData.Query;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ODataETagTest.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get(ODataQueryOptions oDataOptions)
+        {
+            return new[] { "value1", "value2" };
+        }
+    }
+}
